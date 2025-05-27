25ed4baea4b36157b7267def6d7c767b90d0ee39
Hadi <alihady530@gmail.com>
2025-05-27 11:33:54 +0300

    added models and data category

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Choose a Category')),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
