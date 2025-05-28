5395a799fdf91c4d29a5d7a41b8180da64009fa2
Hadi <alihady530@gmail.com>
2025-05-28 15:30:06 +0300

    added more filters

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -9,6 +9,9 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   var _isGlutenFree = false;
+  var _isLactoseFree = false;
+  var _isVegan = false;
+  var _isVegetarian = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +25,78 @@ class _FilterScreenState extends State<FilterScreen> {
                 _isGlutenFree = checked;
               });
             },
-            title: Text('Gluten-Free'),
-            subtitle: Text('Display meals with gluten-free'),
+            title: Text(
+              'Gluten-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              'Display meals with gluten-free',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+          SwitchListTile(
+            value: _isLactoseFree,
+            onChanged: (checked) {
+              setState(() {
+                _isLactoseFree = checked;
+              });
+            },
+            title: Text(
+              'Lactose-Free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              'Display meals with Lactose-free',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+          SwitchListTile(
+            value: _isVegan,
+            onChanged: (checked) {
+              setState(() {
+                _isVegan = checked;
+              });
+            },
+            title: Text(
+              'Vegan',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              'Display Vegan meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+          SwitchListTile(
+            value: _isVegetarian,
+            onChanged: (checked) {
+              setState(() {
+                _isVegetarian = checked;
+              });
+            },
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              'Display Vegetarian meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
           ),
         ],
       ),
