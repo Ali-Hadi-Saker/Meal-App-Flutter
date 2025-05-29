32f3689a02ceae3103e6f1aad051d6c38c22bb76
Hadi <alihady530@gmail.com>
2025-05-29 16:43:25 +0300

    added favorite meals provider

---
+++ lib/providers/favorite_meals_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meal_app/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  //initiate the initial state of the object witch is empty list in this case
+  FavoriteMealsNotifier() : super([]);
+
+  //state hold the data value
+  void toggleFavoriteMealStatus(Meal meal) {
+    final isMealExist = state.contains(meal);
+
+    if (isMealExist) {
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
+
+//initialize StateNotifierProvider object
+//StateNotifierProvider need a Notifier class to work with
+final favoriteMealsProvider =
+    //StateNotifierProvider has two generic types, first is the Notifier class and second is the data type that the Notifier will hold
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+      return FavoriteMealsNotifier();
+    });
--- lib/providers/meal_provider.dart
+++ lib/providers/meal_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meal_app/data/dummy_data.dart';
 
+//Provider is used for static data like in this example where list of meals never change
 final mealProvider = Provider((ref) {
   return dummyMeals;
 });
